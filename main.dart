afb0d8b7a198f31492c81850493beb4a338ec2f7
sultanhussein20 <s.derawi66@gmail.com>
2025-04-21 21:30:30 +0300

    update

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  String myname ="sultan";
+
+int myage =23;
+double myhighest = -179.4;
+int myyear =2002; 
+print(myname[0].toUpperCase());
+print (myhighest.floor());
+print(myhighest.ceil());
+print(myhighest.round());
+print(myhighest.isNegative);
+print(myhighest.abs());
+int myageAfter10YEar=myage+10;
+print("my age after 10 years is $myageAfter10YEar");
+}
